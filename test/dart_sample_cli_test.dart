6e7b0617ac945d790a25212d02c1766f52527921
Michael Owolabi <michael.i.owolabi@gmail.com>
2019-11-20 22:02:53 +0100

    Update dart_sample_cli_test.dart

--- test/dart_sample_cli_test.dart
+++ test/dart_sample_cli_test.dart
@@ -1,8 +1,8 @@
 import 'package:dart_sample_cli/dart_sample_cli.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
